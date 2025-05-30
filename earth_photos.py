06d184e2bc654ce738821d40d7d6366f49e28c31
IrinaQA423 <irina-koshina@mail.ru>
2025-05-30 23:35:19 +0800

    Update enumerate

--- earth_photos.py
+++ earth_photos.py
@@ -13,10 +13,10 @@ def download_earth_photos(api_key, num_photos, save_dir='images'):
     response.raise_for_status()
     nasa_earth_photos = response.json()
 
-    for index, nasa_earth_photos in enumerate(nasa_earth_photos[:num_photos]):
+    for index, nasa_earth_photos in enumerate(nasa_earth_photos[:num_photos], start=1):
 
         image_date = datetime.strptime(nasa_earth_photos['date'], "%Y-%m-%d %H:%M:%S")
         date_str = image_date.strftime("%Y/%m/%d")
         image_filename = nasa_earth_photos['image']
         photo_url = f"https://epic.gsfc.nasa.gov/archive/natural/{date_str}/png/{image_filename}.png"
-        download_image(photo_url, f'earth_photo_{i + 1}', save_dir)
+        download_image(photo_url, f'earth_photo_{i}', save_dir)
