d4ec9ea446ec0890846ee5acd651f7e232c43fed
IrinaQA423 <irina-koshina@mail.ru>
2025-05-31 18:17:43 +0800

    Update earth_photos.py

--- earth_photos.py
+++ earth_photos.py
@@ -19,4 +19,4 @@ def download_earth_photos(api_key, num_photos, save_dir='images'):
         date_str = image_date.strftime("%Y/%m/%d")
         image_filename = nasa_earth_photos['image']
         photo_url = f"https://epic.gsfc.nasa.gov/archive/natural/{date_str}/png/{image_filename}.png"
-        download_image(photo_url, f'earth_photo_{i}', save_dir)
+        download_image(photo_url, f'earth_photo_{index}', save_dir)
